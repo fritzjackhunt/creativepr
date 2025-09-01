cdd39cc677ec3679a73ddfa75fc9267b607cfdb4
Aniekan Essiet <essiet.aniekan@yahoo.com>
2025-09-01 17:38:25 +0300

    dashboard not

--- creative/mypr/urls.py
+++ creative/mypr/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='homepage'),
+    path('campaign', views.campaign, name='campaign'),
+    path('pricing', views.pricing, name='pricing'),
+    path('forbrands', views.forbrands, name='forbrands'),
 ]
--- creative/mypr/views.py
+++ creative/mypr/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'creative/index.html')
+
+def campaign(request):
+    return render(request, 'creative/campaign.html')
+
+def pricing(request):
+    return render(request, 'creative/pricing.html')
+
+def forbrands(request):
+    return render(request, 'creative/forbrands.html')
